3a90c2dfc49ad6c328da81fa54620a62b5d95abd
admin <admin@codelens.com>
2025-12-15 11:58:04 +0000

    Auto-generate unit tests for src/main changes

--- src/test/InvertBinaryTests.py
+++ src/test/InvertBinaryTests.py
@@ -19,34 +19,35 @@ class TestInvertBinaryTree(unittest.TestCase):
     def test_example_tree(self):
         root = create_example_tree()
         inverted_root = invert_tree(root)
+        
+        self.assertEqual(inverted_root.val, 1)
+        self.assertEqual(inverted_root.left.val, 3)
+        self.assertEqual(inverted_root.right.val, 2)
+        self.assertEqual(inverted_root.right.left.val, 5)
+        self.assertEqual(inverted_root.right.right.val, 4)
 
-        expected_inorder = [3, 1, 5, 2, 4]
-        actual_inorder = []
-
-        def inorder_traversal(node):
-            if node:
-                inorder_traversal(node.left)
-                actual_inorder.append(node.val)
-                inorder_traversal(node.right)
-
-        inorder_traversal(inverted_root)
-        self.assertEqual(actual_inorder, expected_inorder)
-
+    def test_already_inverted_tree(self):
+        root = Node(1, Node(3), Node(2, Node(5), Node(4)))
+        inverted_root = invert_tree(root)
+        
+        self.assertEqual(inverted_root.val, 1)
+        self.assertEqual(inverted_root.left.val, 2)
+        self.assertEqual(inverted_root.right.val, 3)
+        self.assertEqual(inverted_root.left.left.val, 4)
+        self.assertEqual(inverted_root.left.right.val, 5)
+        
     def test_complete_binary_tree(self):
         root = Node(1, Node(2, Node(4), Node(5)), Node(3, Node(6), Node(7)))
         inverted_root = invert_tree(root)
 
-        expected_inorder = [7, 3, 6, 1, 5, 2, 4]
-        actual_inorder = []
+        self.assertEqual(inverted_root.val, 1)
+        self.assertEqual(inverted_root.left.val, 3)
+        self.assertEqual(inverted_root.right.val, 2)
+        self.assertEqual(inverted_root.left.left.val, 7)
+        self.assertEqual(inverted_root.left.right.val, 6)
+        self.assertEqual(inverted_root.right.left.val, 5)
+        self.assertEqual(inverted_root.right.right.val, 4)
 
-        def inorder_traversal(node):
-            if node:
-                inorder_traversal(node.left)
-                actual_inorder.append(node.val)
-                inorder_traversal(node.right)
-
-        inorder_traversal(inverted_root)
-        self.assertEqual(actual_inorder, expected_inorder)
 
 if __name__ == '__main__':
     unittest.main()
