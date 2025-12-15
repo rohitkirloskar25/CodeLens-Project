8cc7e54b9d07045ba9ad473c676a7ed693b19e2f
admin <admin@codelens.com>
2025-12-15 12:11:44 +0000

    Auto-generate unit tests for src/main changes

--- src/test/InvertBinaryTests.py
+++ src/test/InvertBinaryTests.py
@@ -20,34 +20,33 @@ class TestInvertBinaryTree(unittest.TestCase):
         root = create_example_tree()
         inverted_root = invert_tree(root)
 
-        expected_inorder = [3, 1, 5, 2, 4]
-        actual_inorder = []
+        self.assertEqual(inverted_root.val, 1)
+        self.assertEqual(inverted_root.left.val, 3)
+        self.assertEqual(inverted_root.right.val, 2)
+        self.assertEqual(inverted_root.right.left.val, 5)
+        self.assertEqual(inverted_root.right.right.val, 4)
 
-        def inorder_traversal(node):
-            if node:
-                inorder_traversal(node.left)
-                actual_inorder.append(node.val)
-                inorder_traversal(node.right)
-
-        inorder_traversal(inverted_root)
-        self.assertEqual(actual_inorder, expected_inorder)
-    
     def test_already_inverted_tree(self):
         root = Node(1, Node(3), Node(2, Node(5), Node(4)))
         inverted_root = invert_tree(root)
 
-        expected_inorder = [4, 2, 5, 1, 3]
-        actual_inorder = []
+        self.assertEqual(inverted_root.val, 1)
+        self.assertEqual(inverted_root.left.val, 2)
+        self.assertEqual(inverted_root.right.val, 3)
+        self.assertEqual(inverted_root.left.left.val, 4)
+        self.assertEqual(inverted_root.left.right.val, 5)
 
-        def inorder_traversal(node):
-            if node:
-                inorder_traversal(node.left)
-                actual_inorder.append(node.val)
-                inorder_traversal(node.right)
+    def test_complete_binary_tree(self):
+        root = Node(1, Node(2, Node(4), Node(5)), Node(3, Node(6), Node(7)))
+        inverted_root = invert_tree(root)
 
-        inorder_traversal(inverted_root)
-        self.assertEqual(actual_inorder, [4, 2, 5, 1, 3])
-
+        self.assertEqual(inverted_root.val, 1)
+        self.assertEqual(inverted_root.left.val, 3)
+        self.assertEqual(inverted_root.right.val, 2)
+        self.assertEqual(inverted_root.left.left.val, 7)
+        self.assertEqual(inverted_root.left.right.val, 6)
+        self.assertEqual(inverted_root.right.left.val, 5)
+        self.assertEqual(inverted_root.right.right.val, 4)
 
 if __name__ == '__main__':
     unittest.main()
